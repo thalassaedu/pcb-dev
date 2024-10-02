b73a10f73af6a96b3cf2ed08bd81bb2c36f090b2
shubham-07k <shubham.kumar@thalassaedu.com>
2024-10-02 17:19:01 +0530

    Light sensor code

---
+++ Code/Arduino/Light_sensor.ino
@@ -0,0 +1,32 @@
+//  This is a prototype code which uses a photoresistor as a light sensor. Currently the code is supported for a very small LED (<5V).
+const int sensorPin = A0;   // Reads the analog value of the photoresistor
+
+const int ledPin = 9;   // Digital PWM pin
+
+int sensorValue;
+
+int threshold = 500;    // Threshold value below which LED brightens
+
+void setup()
+{
+    pinMode(ledPin, OUTPUT);
+    Serial.begin(9600);
+}
+
+void loop()
+{
+    sensorValue = analogRead(sensorPin);
+    Serial.println(sensorValue);
+
+    if (sensorValue < threshold)
+    {
+        analogWrite(ledPin, 255);   // 255 - HIGH
+    }
+
+    else
+    {
+        analogWrite(ledPin, 50);    // 50 - LOW
+    }
+
+    delay(100); // Small delay to stabilise the entire process.
+}
